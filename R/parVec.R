1972955f6e9b669f43eeeb36188b06af612b8dad
JensPederM <jenspederm@gmail.com>
2020-03-03 16:06:29 +0100

    Cleaned after merge fuck-up

--- R/parVec.R
+++ R/parVec.R
@@ -3,11 +3,7 @@
 parVec <- function(cl = NULL, x, FUN, ...) {
   require(parallel, quietly = T)
   if (is.null(cl)) {
-<<<<<<< HEAD
     ncl <- getOption("mc.cores", max(parallel::detectCores(), 1, na.rm=TRUE))
-=======
-    ncl <- getOption("mc.cores", max(parllel::detectCores(), 1, na.rm=TRUE))
->>>>>>> 5bc19f9f58fc3bb7a14131defc399cc59a2b9d2b
     cl <- parallel::makeCluster(ncl)
     on.exit(parallel::stopCluster(cl))
   } else {
