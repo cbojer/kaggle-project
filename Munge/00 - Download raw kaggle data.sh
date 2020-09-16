17b6a4f58427bbad43d566b6265524ba1b4b6df7
JenspederM <37183160+JenspederM@users.noreply.github.com>
2020-09-16 11:05:57 +0200

    Include downloads
    Downloads were commented out for analysis purposes

--- Munge/00 - Download raw kaggle data.sh
+++ Munge/00 - Download raw kaggle data.sh
@@ -8,9 +8,9 @@ export PATH="~/.local/bin:$PATH"
 source ~/.bashrc
 
 # Download Kaggle Data
-#kaggle competitions download favorita-grocery-sales-forecasting -p Data/Raw_Data
-#kaggle competitions download recruit-restaurant-visitor-forecasting -p Data/Raw_Data
+kaggle competitions download favorita-grocery-sales-forecasting -p Data/Raw_Data
+kaggle competitions download recruit-restaurant-visitor-forecasting -p Data/Raw_Data
 kaggle competitions download rossmann-store-sales -p Data/Raw_Data
-#kaggle competitions download walmart-recruiting-store-sales-forecasting -p Data/Raw_Data
-#kaggle competitions download walmart-recruiting-sales-in-stormy-weather -p Data/Raw_Data
-#kaggle competitions download web-traffic-time-series-forecasting -p Data/Raw_Data
+kaggle competitions download walmart-recruiting-store-sales-forecasting -p Data/Raw_Data
+kaggle competitions download walmart-recruiting-sales-in-stormy-weather -p Data/Raw_Data
+kaggle competitions download web-traffic-time-series-forecasting -p Data/Raw_Data
